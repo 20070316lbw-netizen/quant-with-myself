9891518b87f2bb77107eb91116d22878cd6202ed
20070316lbw-netizen <244884540+20070316lbw-netizen@users.noreply.github.com>
2026-05-15 00:09:04 +0800

    add: plan to write something about model

--- model/train.py
+++ model/train.py
@@ -1,3 +1,11 @@
+import sys
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
+
+from utils.config import get_db, load_config
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+
